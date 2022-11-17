1486ee7b3b54857fc4f5f75ae4c30dcde5ecfa56
joaoalmeidas <joao182cx@gmail.com>
2022-11-17 08:48:41 +0000

    Test of the Account class

--- 3.11 Modified Account Class/src/AccountTest.java
+++ 3.11 Modified Account Class/src/AccountTest.java
@@ -5,10 +5,24 @@ public class AccountTest{
         Account account1 = new Account("John", 1000);
         Account account2 = new Account("William", 2000);
 
-        account1.withdraw(50);
-        account2.withdraw(78);
+        System.out.printf("Balance of account1 before withdraw: %.2f%n", account1.getBalance());
+        System.out.printf("Balance of account2 before withdraw: %.2f%n", account2.getBalance());
 
-        System.out.printf("Balance of account1: %f/n", account1.getBalance());
-        System.out.printf("Balance of account2: %f/n", account2.getBalance());
+        System.out.println("Withdraw 50.0 from both accounts");
+
+        account1.withdraw(50);
+        account2.withdraw(50);
+
+        System.out.printf("Balance of account1: %.2f%n", account1.getBalance());
+        System.out.printf("Balance of account2: %.2f%n", account2.getBalance());
+
+        System.out.println("Withdraw 1000.0 from both accounts");
+
+        account1.withdraw(1000);
+        account2.withdraw(1000);
+
+        System.out.printf("Balance of account1: %.2f%n", account1.getBalance());
+        System.out.printf("Balance of account2: %.2f%n", account2.getBalance());
+
     }   
 }
